72a8b5fd1fdbdab251358416650c1f737ecdab6a
woutdenolf <woutdenolf@users.sf.net>
2022-05-23 09:03:07 +0200

    expand ewoks app: convert workflow

---
+++ ewoksjob/tests/test_convert.py
@@ -0,0 +1,22 @@
+from ..client import celery
+from ..client import process
+from .utils import get_result
+
+
+def test_convert(celery_session_worker, tmpdir):
+    assert_convert(celery, tmpdir)
+
+
+def test_convert_local(tmpdir):
+    with process.pool_context():
+        assert_convert(process, tmpdir)
+
+
+def assert_convert(mod, tmpdir):
+    filename = tmpdir / "test.json"
+    args = {"graph": {"idd": "testgraph", "version": "1.0"}}, str(filename)
+    kwargs = {"save_options": {"indent": 2}}
+    future = mod.convert_workflow(args=args, kwargs=kwargs)
+    results = get_result(future, timeout=3)
+    assert results is None
+    assert filename.exists()
