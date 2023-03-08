b670168cb8b5c47276d6308a4b0bd68dd0a3c8aa
woutdenolf <woutdenolf@users.sf.net>
2023-03-08 10:26:42 +0100

    clarify code

--- src/ewoksjob/worker/errors.py
+++ src/ewoksjob/worker/errors.py
@@ -25,25 +25,29 @@ def replace_exception_for_client():
     try:
         yield
     except Exception as e:
-        if _client_has_exception(e):
+        if _client_can_deserialize(e):
             raise
         tb = traceback.format_exc().rstrip()
         cause = EwoksJobExceptionCause(f"\n\n{tb}")
         raise EwoksJobException(str(e)) from cause
     except BaseException as e:
-        if _client_has_exception(e):
+        if _client_can_deserialize(e):
             raise
         tb = traceback.format_exc().rstrip()
         cause = EwoksJobBaseExceptionCause(f"\n\n{tb}")
         raise EwoksJobBaseException(str(e)) from cause
 
 
-def _client_has_exception(exception: BaseException) -> bool:
+def _client_can_deserialize(exception: BaseException) -> bool:
+    """Returns `True` when the client-side will be able to deserialize the Exception object."""
     queue = [exception]
     while queue:
         e = queue.pop()
         emodule = e.__class__.__module__
-        if not any(emodule.startswith(mod) for mod in _CLIENT_EXCEPTION_MODULES):
+        client_can_deserialize = any(
+            emodule.startswith(mod) for mod in _CLIENT_EXCEPTION_MODULES
+        )
+        if not client_can_deserialize:
             return False
         if isinstance(e.__cause__, BaseException):
             queue.append(e.__cause__)
