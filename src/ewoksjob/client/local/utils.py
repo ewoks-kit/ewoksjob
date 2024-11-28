003e1ebc411d5142c874835933701c9e20f04cc0
Loic Huder <loic.huder@esrf.fr>
2024-11-28 10:57:05 +0100

    Local `get_future` return value is not Optional

--- src/ewoksjob/client/local/utils.py
+++ src/ewoksjob/client/local/utils.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 from concurrent.futures import Future
 from .pool import get_active_pool
 
@@ -12,7 +12,7 @@ __all__ = [
 ]
 
 
-def get_future(task_id) -> Optional[Future]:
+def get_future(task_id) -> Future:
     pool = get_active_pool()
     return pool.get_future(task_id)
 
@@ -20,14 +20,12 @@ def get_future(task_id) -> Optional[Future]:
 def cancel(task_id):
     """The current implementation does not allow cancelling running tasks"""
     future = get_future(task_id)
-    if future is not None:
-        future.cancel()
+    future.cancel()
 
 
 def get_result(task_id, **kwargs):
     future = get_future(task_id)
-    if future is not None:
-        return future.result(**kwargs)
+    return future.result(**kwargs)
 
 
 def get_not_finished_task_ids() -> list:
@@ -38,5 +36,4 @@ def get_not_finished_task_ids() -> list:
 
 def get_not_finished_futures() -> List[Future]:
     """Get all futures that are not finished"""
-    lst = [get_future(task_id) for task_id in get_not_finished_task_ids()]
-    return [future for future in lst if future is not None]
+    return [get_future(task_id) for task_id in get_not_finished_task_ids()]
