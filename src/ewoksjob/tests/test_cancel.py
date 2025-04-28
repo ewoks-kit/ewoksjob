f665b838928ac9bf48af70a9a6ffed609c20e654
woutdenolf <woutdenolf@users.sf.net>
2025-04-28 10:33:50 +0200

    split _assert_cancel

--- src/ewoksjob/tests/test_cancel.py
+++ src/ewoksjob/tests/test_cancel.py
@@ -5,8 +5,8 @@ import pytest
 
 from ..client import celery
 from ..client import local
-from ..client.futures import TimeoutError
-from ..client.futures import CancelledError
+from ..client import TimeoutError
+from ..client import CancelledError
 
 from .utils import wait_not_finished
 
@@ -34,43 +34,54 @@ def _assert_normal(mod, slurm_tmp_path):
 
 
 def test_cancel(ewoks_worker, slurm_tmp_path):
-    can_be_cancelled = sys.platform != "win32"
-    # Cancelling a Celery job on Windows does not work.
-    # https://docs.celeryq.dev/en/stable/faq.html#does-celery-support-windows
-    _assert_cancel(celery, slurm_tmp_path, can_be_cancelled)
+    on_windows = sys.platform == "win32"
+    if on_windows:
+        # Cancelling a Celery job on Windows does not work.
+        # https://docs.celeryq.dev/en/stable/faq.html#does-celery-support-windows
+        _assert_cannot_be_cancelled(celery, slurm_tmp_path)
+    else:
+        _assert_cancel(celery, slurm_tmp_path)
 
 
 def test_cancel_local(local_ewoks_worker, slurm_tmp_path):
-    can_be_cancelled = local.get_active_pool().pool_type == "slurm"
-    _assert_cancel(local, slurm_tmp_path, can_be_cancelled)
+    is_slurm_pool = local.get_active_pool().pool_type == "slurm"
+    if is_slurm_pool:
+        _assert_cancel(local, slurm_tmp_path)
+    else:
+        _assert_cannot_be_cancelled(local, slurm_tmp_path)
 
 
-def _assert_cancel(mod, slurm_tmp_path, can_be_cancelled):
+def _assert_cancel(mod, slurm_tmp_path):
     filename = slurm_tmp_path / "finished.smf"
     future = mod.submit_test(seconds=12, filename=str(filename))
     wait_not_finished(mod, {future.uuid}, timeout=3)
 
-    if can_be_cancelled:
-        _ = mod.cancel(future.uuid)
+    _ = mod.cancel(future.uuid)
 
-        # Check whether the job is cancelled.
-        with pytest.raises(CancelledError):
-            # Do not use future.result() so we test geting the result from the uuid.
-            _ = mod.get_result(future.uuid, timeout=30)
+    # Check whether the job is cancelled.
+    with pytest.raises(CancelledError):
+        # Do not use future.result() so we test geting the result from the uuid.
+        _ = mod.get_result(future.uuid, timeout=30)
 
-        _nfs_cache_refresh(filename)
-        assert not filename.exists()
-    else:
-        _ = mod.cancel(future.uuid)
+    _nfs_cache_refresh(filename)
+    assert not filename.exists()
 
-        # Check that the job cannot be cancelled.
-        with pytest.raises(TimeoutError):
-            # Do not use future.result() so we test geting the result from the uuid.
-            _ = mod.get_result(future.uuid, timeout=3)
 
-        _ = future.result(timeout=30)
-        _nfs_cache_refresh(filename)
-        assert filename.exists()
+def _assert_cannot_be_cancelled(mod, slurm_tmp_path):
+    filename = slurm_tmp_path / "finished.smf"
+    future = mod.submit_test(seconds=12, filename=str(filename))
+    wait_not_finished(mod, {future.uuid}, timeout=3)
+
+    _ = mod.cancel(future.uuid)
+
+    # Check that the job cannot be cancelled.
+    with pytest.raises(TimeoutError):
+        # Do not use future.result() so we test geting the result from the uuid.
+        _ = mod.get_result(future.uuid, timeout=3)
+
+    _ = future.result(timeout=30)
+    _nfs_cache_refresh(filename)
+    assert filename.exists()
 
 
 def _nfs_cache_refresh(filename: pathlib.Path):
