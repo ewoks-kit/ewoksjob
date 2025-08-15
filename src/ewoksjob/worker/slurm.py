c4881eeffaa5ba5e5976d85349bdfd2da6832653
woutdenolf <woutdenolf@users.sf.net>
2025-08-15 11:20:49 +0200

    slurm: fix warm shutdown

--- src/ewoksjob/worker/slurm.py
+++ src/ewoksjob/worker/slurm.py
@@ -7,9 +7,13 @@ from functools import wraps
 from typing import Callable, Any, Optional
 
 try:
+    import gevent
     from gevent import GreenletExit
 except ImportError:
-    GreenletExit = None
+    # Avoid error one import. Do cause error when actually trying to use this pool.
+    gevent = NotImplemented
+    GreenletExit = NotImplemented
+
 from celery.concurrency.gevent import TaskPool as _TaskPool
 
 try:
@@ -39,12 +43,23 @@ class TaskPool(_TaskPool):
         self._create_slurm_executor()
 
     def restart(self):
-        self._remove_slurm_executor()
+        self._safe_remove_slurm_executor()
         self._create_slurm_executor()
 
     def on_stop(self):
-        self._remove_slurm_executor()
+        self._safe_remove_slurm_executor()
         super().on_stop()
+
+    def _safe_remove_slurm_executor(self):
+        if self._is_in_gevent_callback():
+            g = gevent.spawn(self._remove_slurm_executor)
+            g.join()
+        else:
+            self._remove_slurm_executor()
+
+    def _is_in_gevent_callback(self):
+        hub = gevent.get_hub()
+        return gevent.getcurrent() is hub
 
     def _create_slurm_executor(self):
         maxtasksperchild = self.options["maxtasksperchild"]
