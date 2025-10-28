4f99fc10423f7d2648e9df26b57ddf8b20454b7f
woutdenolf <woutdenolf@users.sf.net>
2025-10-28 14:32:54 +0100

    rename variable

--- src/ewoksjob/worker/process.py
+++ src/ewoksjob/worker/process.py
@@ -144,13 +144,13 @@ class TaskPool(base.BasePool):
         error_callback=None,
         **_,
     ):
-        f = self._safe_submit(target, args, kwargs, accept_callback)
+        future = self._safe_submit(target, args, kwargs, accept_callback)
 
         if callback is not None or error_callback is not None:
 
-            def done_callback(f):
+            def done_callback(future):
                 try:
-                    result = f.result()
+                    result = future.result()
                 except BrokenProcessPool:
                     # Child process was killed
                     # TODO: prefork.process_destructor(pid, exitcode)
@@ -168,8 +168,8 @@ class TaskPool(base.BasePool):
                     if callback is not None:
                         callback(result)
 
-        f.add_done_callback(done_callback)
-        return ApplyResult(f)
+        future.add_done_callback(done_callback)
+        return ApplyResult(future)
 
     def _safe_submit(self, target, args, kwargs, accept_callback) -> Future:
         while True:
