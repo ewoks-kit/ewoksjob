95568cc49cbdb88c09de22efee6262e5253b4dfa
Thomas VINCENT <thomas.vincent@esrf.fr>
2026-07-02 10:39:51 +0200

    split tests

--- src/ewoksjob/tests/test_task_utils.py
+++ src/ewoksjob/tests/test_task_utils.py
@@ -2,48 +2,95 @@ import pytest
 
 from ..client.task_utils import TaskSubmitter
 
-EXECUTION_MODES = "celery", "thread"  # , "process", "slurm"
+TASK_TESTS = [
+    ("ewokscore.tests.examples.tasks.addfunc.addfunc", (), {"arg": 2}, 3),
+    ("builtins.max", (1, 2, -1), {}, 2),
+    (
+        "ewokscore.tests.examples.tasks.sumtask.SumTask",
+        (),
+        {"a": 1, "b": 2},
+        {"result": 3},
+    ),
+]
 
 
-@pytest.mark.parametrize("execution_mode", EXECUTION_MODES)
-def test_function_submitter(execution_mode, ewoks_worker):
-    submit_addfunc = TaskSubmitter(
-        "ewokscore.tests.examples.tasks.addfunc.addfunc", execution_mode=execution_mode
-    )
-    assert submit_addfunc(arg=2).result(timeout=10) == 3
+@pytest.mark.parametrize("task,args,kwargs,result", TASK_TESTS)
+def test_task_submitter_worker(ewoks_worker, task, args, kwargs, result):
+    submitter = TaskSubmitter(task, execution_mode="celery")
+    assert submitter(*args, **kwargs).result(timeout=10) == result
 
 
-@pytest.mark.parametrize("execution_mode", EXECUTION_MODES)
-def test_builtin_function_submitter(execution_mode, ewoks_worker):
-    submit_max = TaskSubmitter("builtins.max", execution_mode=execution_mode)
-    assert submit_max(1, 2, -1).result(timeout=10) == 2
+@pytest.mark.parametrize("task,args,kwargs,result", TASK_TESTS)
+def test_task_submitter_thread(task, args, kwargs, result):
+    submitter = TaskSubmitter(task, execution_mode="thread")
+    assert submitter(*args, **kwargs).result(timeout=10) == result
 
 
-@pytest.mark.parametrize("execution_mode", EXECUTION_MODES)
-def test_task_submitter(execution_mode, ewoks_worker):
-    submit_sum_task = TaskSubmitter(
-        "ewokscore.tests.examples.tasks.sumtask.SumTask", execution_mode=execution_mode
-    )
-    assert submit_sum_task(a=1, b=2).result(timeout=10) == {"result": 3}
+@pytest.mark.parametrize("task,args,kwargs,result", TASK_TESTS)
+def test_task_submitter_process(skip_if_gevent, task, args, kwargs, result):
+    submitter = TaskSubmitter(task, execution_mode="process")
+    assert submitter(*args, **kwargs).result(timeout=10) == result
 
 
-@pytest.mark.parametrize("execution_mode", EXECUTION_MODES)
-def test_function_from_script_submitter(execution_mode, tmp_path, ewoks_worker):
+def test_function_from_script_submitter_worker(ewoks_worker, tmp_path):
     script_path = tmp_path / "test.py"
     script_path.write_text("def function(a, b): return a + b\n")
 
     submit_script_function = TaskSubmitter(
-        f"{script_path}::function", execution_mode=execution_mode
+        f"{script_path}::function", execution_mode="celery"
     )
     assert submit_script_function(a=1, b=2).result(timeout=10) == 3
 
 
-@pytest.mark.parametrize("execution_mode", EXECUTION_MODES)
-def test_script_submitter(execution_mode, tmp_path, ewoks_worker):
+def test_function_from_script_submitter_thread(tmp_path):
+    script_path = tmp_path / "test.py"
+    script_path.write_text("def function(a, b): return a + b\n")
+
+    submit_script_function = TaskSubmitter(
+        f"{script_path}::function", execution_mode="thread"
+    )
+    assert submit_script_function(a=1, b=2).result(timeout=10) == 3
+
+
+def test_function_from_script_submitter_process(skip_if_gevent, tmp_path):
+    script_path = tmp_path / "test.py"
+    script_path.write_text("def function(a, b): return a + b\n")
+
+    submit_script_function = TaskSubmitter(
+        f"{script_path}::function", execution_mode="thread"
+    )
+    assert submit_script_function(a=1, b=2).result(timeout=10) == 3
+
+
+def test_script_submitter_worker(ewoks_worker, tmp_path):
     script_path = tmp_path / "test.py"
     script_path.write_text("import sys; sys.exit(2)")
 
-    submit_script = TaskSubmitter(str(script_path), execution_mode=execution_mode)
+    submit_script = TaskSubmitter(str(script_path), execution_mode="celery")
+    assert submit_script().result(timeout=10) == {
+        "err": None,
+        "out": None,
+        "return_code": 2,
+    }
+
+
+def test_script_submitter_thread(tmp_path):
+    script_path = tmp_path / "test.py"
+    script_path.write_text("import sys; sys.exit(2)")
+
+    submit_script = TaskSubmitter(str(script_path), execution_mode="thread")
+    assert submit_script().result(timeout=10) == {
+        "err": None,
+        "out": None,
+        "return_code": 2,
+    }
+
+
+def test_script_submitter_process(skip_if_gevent, tmp_path):
+    script_path = tmp_path / "test.py"
+    script_path.write_text("import sys; sys.exit(2)")
+
+    submit_script = TaskSubmitter(str(script_path), execution_mode="process")
     assert submit_script().result(timeout=10) == {
         "err": None,
         "out": None,
